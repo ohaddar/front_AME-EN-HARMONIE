5de091e744fc9c4bb66addb586b8ab893d8693bd
Oumaima <oumaima.haddar.majdeb@gmail.com>
2025-01-18 23:09:30 +0100

    📝 🗑️ update questionnaire and remove fake test data

--- src/hooks/useQuestionnaire.ts
+++ src/hooks/useQuestionnaire.ts
@@ -2,20 +2,23 @@
 import { useEffect, useState } from "react";
 import QuestionService from "../api/QuestionService";
 import { Question } from "../types/types";
+import { useAuth } from "../contexts/AuthContext";
+import axios from "axios";
 
 export const useQuestionnaire = () => {
   const [questionService] = useState(new QuestionService());
   const [currentQuestion, setCurrentQuestion] = useState<Question | null>(null);
+  const [questionnaireId, setQuestionnaireId] = useState<string | null>(null); // State to store the questionnaire ID
+
   const [resultMessage, setResultMessage] = useState<string | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
-
+  const { currentUser } = useAuth();
   useEffect(() => {
     const loadQuestionnaire = async () => {
       try {
         const response = await questionService.loadQuestionnaire();
-        console.log("response", response);
-
+        setQuestionnaireId(response.id);
         const firstQuestion = questionService.getQuestionById("0.1");
         setCurrentQuestion(firstQuestion || null);
       } catch (err) {
@@ -29,16 +32,45 @@ export const useQuestionnaire = () => {
     loadQuestionnaire();
   }, [questionService]);
 
-  const handleAnswer = (answer: string) => {
+  const handleAnswer = async (answer: string) => {
     if (!currentQuestion) return;
 
     try {
       const next = questionService.getNextQuestionById(
         currentQuestion.id,
-        answer
+        answer,
       );
       if (typeof next === "string") {
-        setResultMessage(next);
+        const resultMessage = next;
+        const testResult = {
+          id: 123,
+          description: resultMessage,
+          datetime: new Date().toISOString(),
+          user: {
+            id: currentUser?.id,
+          },
+          questionnaireId: questionnaireId,
+        };
+
+        const token = localStorage.getItem("token");
+
+        if (!token) {
+          setError("Authentication token is missing or expired.");
+          return;
+        }
+
+        try {
+          await axios.post("http://localhost:8080/results/save", testResult, {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+          });
+          console.log("Result successfully sent to the server");
+        } catch (apiError) {
+          console.error("Error saving result to the server:", apiError);
+        }
+        setResultMessage(resultMessage);
         setCurrentQuestion(null);
       } else {
         setCurrentQuestion(next || null);
