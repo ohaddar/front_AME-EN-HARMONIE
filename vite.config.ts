0698ea2e52f859e604c2a92c9d4713dfe63d050d
Oumaima <oumaima.haddar.majdeb@gmail.com>
2025-02-07 15:33:32 +0100

    💄 update Vite configuration for testing support and add necessary dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
@@ -8,5 +8,10 @@ export default defineConfig({
   },
   optimizeDeps: {
     include: ["react", "react-dom"],
+  },
+  test: {
+    globals: true, // Global testing utilities (like `expect`)
+    environment: "jsdom", // Use jsdom as a browser-like environment
+    setupFiles: "./src/__tests__/setupTests.ts",
   },
 });
