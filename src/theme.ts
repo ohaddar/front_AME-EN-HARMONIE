756992a4bfe7d7e8c8e0a91e343e220f1c81d521
Oumaima <oumaima.haddar.majdeb@gmail.com>
2025-02-13 08:35:26 +0100

    💄 update theme, override menu item styles

--- src/theme.ts
+++ src/theme.ts
@@ -3,7 +3,7 @@ import { createTheme } from "@mui/material/styles";
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#9f7aea",
+      main: "rgb(79, 37, 146)",
       dark: "#7e5bb5",
     },
     secondary: {
@@ -25,7 +25,7 @@ const theme = createTheme({
       styleOverrides: {
         root: {
           color: "#f0f0f0",
-          backgroundColor: "#9f7aea",
+          backgroundColor: "rgb(79, 37, 146)",
           "&:hover": {
             backgroundColor: "#7e5bb5",
           },
@@ -37,6 +37,33 @@ const theme = createTheme({
         root: {
           color: "rgb(79, 37, 146)",
           padding: "0px",
+        },
+      },
+    },
+    MuiMenuItem: {
+      styleOverrides: {
+        root: {
+          color: "rgb(79, 37, 146)",
+          position: "relative",
+          transition: "color 0.4s ease",
+          "&::after": {
+            content: '""',
+            position: "absolute",
+            left: 0,
+            bottom: 2,
+            width: 0,
+            height: "2px",
+            backgroundColor: "rgb(79, 37, 146)",
+            transition: "width 0.4s ease, left 0.4s ease",
+          },
+          "&:hover": {
+            backgroundColor: "inherit",
+            fontWeight: "bold",
+            "&::after": {
+              width: "60%",
+              left: "20%",
+            },
+          },
         },
       },
     },
